a067449b76ab7935eda8246161c63923c7cf4614
BykovIA <igor.bykov.96@bk.ru>
2018-10-10 23:18:31 +0500

    Добавил classwork3

---
+++ classwork3/classwork3.R
@@ -0,0 +1,9 @@
+
+#Загружены данные о землятресениях
+anss <- readLines("https://raw.githubusercontent.com/SergeyMirvoda/MD-DA-2017/master/data/earthquakes_2011.html", warn=FALSE)
+choosing_strings <- grep ("^\\d+/\\d+/\\d+\\s\\d+:\\d+:\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,\\w+,\\d+,,,-?\\d+\\.\\d+,\\w+,\\d+$", anss, value=TRUE)
+choosing_strings
+
+#Проверено что все строки (all.equal) в результирующем векторе подходят под шаблон. 
+all.equal(choosing_strings,"^\\d+/\\d+/\\d+\\s\\d+:\\d+:\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,-?\\d+\\.\\d+,\\w+,\\d+,,,-?\\d+\\.\\d+,\\w+,\\d+$")
+
